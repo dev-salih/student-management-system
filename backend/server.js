bee1c531e23f5f3c25e35bcd0a3710db94d3a298
dev-salih <salihmu79@gmail.com>
2026-05-28 22:24:24 +0300

    data stored into variables using object destructuring

--- backend/server.js
+++ backend/server.js
@@ -8,58 +8,59 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+    res.send("Server is running");
 });
 
 // DATABASE CONNECTION
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "student_management",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "student_management",
 });
 
 // CONNECT DATABASE
 db.connect((err) => {
-  if (err) {
-    console.log("Connection failed");
-    console.log(err);
-  } else {
-    console.log("Connected to MySQL");
-
-    // Table Creation Query
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS students (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        full_name VARCHAR(100),
-        email VARCHAR(100),
-        phone VARCHAR(20),
-        department VARCHAR(100),
-        address TEXT,
-        gender VARCHAR(20)
-      )
-    `;
-
-    // Execute the Query
-    db.query(createTableQuery, (err, result) => {
-      if (err) {
-        console.log("Table creation failed");
+    if (err) {
+        console.log("Connection failed");
         console.log(err);
-      } else {
-        console.log("Students table created successfully");
-      }
-    });
-  }
+    } else {
+        console.log("Connected to MySQL");
+
+        // Table Creation Query
+        const createTableQuery = `
+            CREATE TABLE IF NOT EXISTS students (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            full_name VARCHAR(100),
+            email VARCHAR(100),
+            phone VARCHAR(20),
+            department VARCHAR(100),
+            address TEXT,
+            gender VARCHAR(20)
+            )
+        `;
+
+        // Execute the Query
+        db.query(createTableQuery, (err, result) => {
+            if (err) {
+                console.log("Table creation failed");
+                console.log(err);
+            } else {
+                console.log("Students table created successfully");
+            }
+        });
+    }
 });
 
 // INSERT API
 app.post("/students", (req, res) => {
-  console.log(req.body);
+    // Store Data into Variables(using "object destructuring")
+    const { full_name, email, phone, department, address, gender } = req.body;
 
-  res.send("API working");
+
 });
 
 // START SERVER
 app.listen(5000, () => {
-  console.log("Server running on port 5000");
+    console.log("Server running on port 5000");
 });
